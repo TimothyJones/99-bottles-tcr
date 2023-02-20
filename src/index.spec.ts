4098adc13d427382085233f21cffa1fe32890238
Timothy Jones <timothy.l.jones@gmail.com>
2023-02-20 16:46:07 +1100

    Added second line of verse.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,8 +4,9 @@ describe('Bottles tests', () => {
   it('has a test', () => {
     // 99 bottles of milk on the wall, 99 bottles of milk.
     // Take one down and pass it around, 98 bottles of milk on the wall.
-    expect(verse(98)).toBe(
-      '98 bottles of milk on the wall, 98 bottles of milk.'
-    );
+    expect(verse(98)).toEqual([
+      '98 bottles of milk on the wall, 98 bottles of milk.',
+      'Take one down and pass it around, 97 bottles of milk on the wall.',
+    ]);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export const verse = (currentBottles: number): string =>
-  `${currentBottles} bottles of milk on the wall, ${currentBottles} bottles of milk.`;
+export const verse = (currentBottles: number): string[] => [
+  `${currentBottles} bottles of milk on the wall, ${currentBottles} bottles of milk.`,
+  `Take one down and pass it around, ${
+    currentBottles - 1
+  } bottles of milk on the wall.`,
+];
